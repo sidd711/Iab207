46d6af8c053e27257c06fc9c0e09bc565c7265db
cookamungus <Kriss1andrew@gmail.com>
2021-11-02 17:36:21 +1000

    update functionality

--- Website/models.py
+++ Website/models.py
@@ -11,10 +11,9 @@ class User(db.Model, UserMixin):
     emailid = db.Column(db.String(100), index=True, nullable=False)
     # password is never stored in the DB, an encrypted password is stored
     # the storage should be at least 255 chars long
-    address = db.Column(db.String, nullable=False)
-
     password_hash = db.Column(db.String(255), nullable=False)
     contact_no = db.Column(db.String(25), nullable=False)
+    
     image = db.Column(db.String(400))
     # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='users')
@@ -78,6 +77,5 @@ class Booking(db.Model):
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     # A booking needs a number of guests attending
     attending = db.Column(db.Integer, nullable=False)
-
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
