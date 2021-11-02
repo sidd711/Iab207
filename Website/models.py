3171984eaab3d0a29fc2249431f017cabf5d7ee9
cookamungus <Kriss1andrew@gmail.com>
2021-11-02 18:01:00 +1000

    user

--- Website/models.py
+++ Website/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     # the storage should be at least 255 chars long
     password_hash = db.Column(db.String(255), nullable=False)
     contact_no = db.Column(db.String(25), nullable=False)
-    
+    address = db.Column(db.String, nullable=False)
     image = db.Column(db.String(400))
     # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='users')
